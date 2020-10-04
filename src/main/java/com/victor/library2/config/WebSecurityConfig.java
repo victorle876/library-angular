12b9153c131610ba110605f266ecf62535fe8ebf
victorle876 <victorl648@gmail.com>
2020-10-04 11:17:39 +0200

    P7- modif dans entitie DTO + rattacher id origine sur celui de la destination

--- src/main/java/com/victor/library2/config/WebSecurityConfig.java
+++ src/main/java/com/victor/library2/config/WebSecurityConfig.java
@@ -56,7 +56,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests().antMatchers("/api/**").permitAll()
-                .antMatchers("/api/test/**").permitAll()
+                .antMatchers("/api/**").permitAll()
+                .antMatchers("/").permitAll()
                 .anyRequest().authenticated();
 
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
